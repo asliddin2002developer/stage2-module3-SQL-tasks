4b00691f7ed86a12f160e8441b233675e202fb3d
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-02-04 12:13:04 +0500

    Database challenge Update

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM Payment WHERE amount >= 500;
-select * from student WHERE DATEDIFF(YEAR, DATEADD(year,1, birthday), '2000-10-10') > 20;
-select * from student where groupnumber=10  AND DATEDIFF(YEAR, DATEADD(year,1, birthday), '2000-10-10') < 20;
-select * from student where name='Mike' OR groupnumber IN (4, 5, 6);
-select * from payment where DATEDIFF(month, DATEADD(year,1, payment_date), '2000-10-10') <= 8;
+select * from student WHERE DATEDIFF(YEAR, birthday, '2023-12-12') > 20;
+select * from student where groupnumber=10  AND DATEDIFF(YEAR, birthday, '2023-12-12') < 20;
+select * from student where name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
+select * from payment where payment_date >= (SELECT DATEADD(month, -8, '2022-08-25') AS DateAdd);
 select * from student where name LIKE 'A%';
-select * from student where name='Roxi' and groupnumber=4 OR name='Tallie' and groupnumber=9;
+select * from student where name LIKE 'Roxi%' and groupnumber=4 OR name LIKE 'Tallie%' and groupnumber=9;
